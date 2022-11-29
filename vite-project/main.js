b483392abb313c502a4cfe018b4d555ac1fbbaf9
Sophie Dong <sophiedong2007@gmail.com>
2022-11-29 10:18:56 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("cool")) {
     document.body.classList.add("warm");
