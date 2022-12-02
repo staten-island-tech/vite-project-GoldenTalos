90e3628b4fe9da92a3c61fa54b074271231bb371
Sophie Dong <sophiedong2007@gmail.com>
2022-12-02 10:18:18 -0500

    :(

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,1 +1,69 @@
+const menu = [
+  {
+    name: "Burger",
+    price: 14,
+    description:
+      "High quality beef, medium well, with cheese and bacon on a multigrain bun",
+    image:
+      "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Bacon-McDouble:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
+    meat: "beef, pork",
+  },
+  {
+    name: "French Fries",
+    price: 5,
+    description: "Long, thin pieces of potato fried in oil; thin and crispy",
+    image:
+      "https://www.mcdonalds.com/content/dam/usa/nfl/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.jpg",
+    meat: "none",
+  },
+  {
+    name: "French Fries",
+    price: 6,
+    description: "Crispy Fries",
+    image:
+      "https://www.thespruceeats.com/thmb/IHKuXcx3uUI1IWkM_cnnQdFH-zQ=/3485x2323/filters:fill(auto,1)/how-to-make-homemade-french-fries-2215971-hero-01-02f62a016f3e4aa4b41d0c27539885c3.jpg",
+    meat: "none",
+  },
+  {
+    name: "Buffalo Wings",
+    price: 9,
+    description: "Spicy Buffalo Wings",
+    image:
+      "https://img.buzzfeed.com/video-api-prod/assets/7def155256434be9ac01e752e1b79558/Facebook_Square.jpg",
+    meat: "chicken",
+  },
+  {
+    name: "Chicken Tenders",
+    price: 8,
+    description: "Crispy Chicken Tenders",
+    image:
+      "https://thecozycook.com/wp-content/uploads/2020/07/Fried-Chicken-Tenders-f.jpg",
+    meat: "chicken",
+  },
+  {
+    name: "Steak",
+    price: 6,
+    description: "Made From Rats Caught in The Kitchen",
+    image:
+      "https://www.seriouseats.com/thmb/lBAvOl5D32f_fge1kx8Asver10Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/butter-basted-pan-seared-steaks-recipe-hero-06-03b1131c58524be2bd6c9851a2fbdbc3.jpg",
+    meat: "beef",
+  },
+  {
+    name: "Caesar Salad",
+    price: 9,
+    description: "Salad with Chicken",
+    image:
+      "https://www.verywellfit.com/thmb/c3anlJTrJwv1nAAhXUvKqgjndTo=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/caesar-salad-with-grilled-chicken-121664016-5bd22cbc46e0fb00519577c3.jpg",
+    meat: "chicken",
+  },
+  {
+    name: "Ice Cream",
+    price: 11,
+    description: "Delicious Ice Cream",
+    image:
+      "https://hips.hearstapps.com/hmg-prod/images/easiest-ever-fruit-and-coconut-ice-cream-1643120950.jpg?crop=0.990xw:0.660xh;0,0.158xh&resize=640:*",
+    meat: "none",
+  },
+];
+
 export { menu };
